f0ff9008e5311cde25394a37af8047cd82a60a27
thaal05 <thalita.almeida.modena@gmail.com>
2026-09-09 22:08:20 -0300

    atualizacao4

---
+++ exercicio1/Main.java
@@ -0,0 +1,14 @@
+//Crie duas variáveis inteiras e mostre qual delas é maior.
+    
+public class Main {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+
+        if (numero1 > numero2) {
+            System.out.println("O maior número é: " + numero1);
+        } else {
+            System.out.println("O maior número é: " + numero2);
+        }
+    }
+}
