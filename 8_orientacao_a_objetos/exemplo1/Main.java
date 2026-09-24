7f28de11d009cd598d6e2d6e5db35fb618c69e34
thaal05 <thalita.almeida.modena@gmail.com>
2026-09-23 22:01:56 -0300

    atualizacao9

--- 8_orientacao_a_objetos/exemplo1/Main.java
+++ 8_orientacao_a_objetos/exemplo1/Main.java
@@ -1,15 +1,19 @@
-public class Main {
+class personagem {
+    String nome = "Heroi";
+    int idade;
+    int poder;
+}
+    
+    public class Main {
+    public static void main(String[] args){
+        personagem persona = new personagem();
 
-    public static void main(String[] args) {
-
-        Personagem persona = new Personagem();
-
-        persona.nome = "Pateta";
         persona.idade = 42;
         persona.poder = 4;
 
         System.out.println(persona.nome);
         System.out.println(persona.idade);
         System.out.println(persona.poder);
+        
     }
-}
+} 
